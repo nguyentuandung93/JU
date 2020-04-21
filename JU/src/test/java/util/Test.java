503eded98f963f3d7d24393e2ce9fa33f0078197
nguyentuandung93 <tuandungbk56@gmail.com>
2020-04-21 15:17:23 +0900

    dung new change

--- JU/src/test/java/util/Test.java
+++ JU/src/test/java/util/Test.java
@@ -1,7 +1,7 @@
 package util;
 
 public class Test {
-	// Add new comment for this project
+	// Dung write comment
 	public static void main(String[] args) {
 		System.out.println("テストファイルを追加");
 	}
---
+++ JU/src/test/java/util/Test2.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Write new line");
+	}
+}
